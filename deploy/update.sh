d5a84d639d5c87ef752d7a34260655e5258c1cea
wheeeung <whee050916@gmail.com>
2021-05-18 19:34:26 +0900

    django

--- deploy/update.sh
+++ deploy/update.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-PROJECT_BASE_PATH='C:\Users\user\PycharmProjects\djangoProject1'
+PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
-supervisorctl restart app
+supervisorctl restart profiles_api
 
 echo "DONE! :)"
